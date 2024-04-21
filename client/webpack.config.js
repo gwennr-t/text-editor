b25653ef18ee83a58368b1fcd55f1dc869c7ed98
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-20 20:59:03 -0500

    made small edits to set up file structures and add to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,9 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
@@ -18,7 +20,7 @@ module.exports = () => {
     plugins: [
         new HtmlWebpackPlugin({
           template: './index.html',
-          title: 'Text Editor'
+          title: 'JATE'
         }),
        
         new InjectManifest({
@@ -30,8 +32,8 @@ module.exports = () => {
         new WebpackPwaManifest({
           fingerprints: false,
           inject: true,
-          name: 'Text Editor',
-          short_name: 'Editor',
+          name: 'Just Another Text Editor',
+          short_name: 'JATE',
           description: 'Happy Editing!',
           background_color: '#225ca3',
           theme_color: '#225ca3',
@@ -49,7 +51,22 @@ module.exports = () => {
     // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
